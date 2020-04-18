ed03bc1853853acfb36755289416680c91bffc1b
ShimmyShaman <blackforestcheesecake@protonmail.ch>
2020-04-19 09:29:52 +1200

    working with demo

--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+    #/home/daniel/cling/obj/lib/libcling.so  \
+    #/home/daniel/cling/lib/libcling.so.5  \
+    #/home/daniel/cling/obj/lib/libcling.so.5.0.0  \
+    #-L/home/daniel/cling/obj/lib                                \
 
 echo
 echo Compiling
+export LD_LIBRARY_PATH='/home/daniel/cling/obj/lib'
 export PKG_CONFIG_PATH='/usr/local/lib64/pkgconfig/'
-g++ -g -o ./bin/midge `pkg-config --cflags glfw3` -lGL -lX11 -lpthread -lXrandr -lXi -ldl -I./src $(find . -type f -iregex ".*\.cpp") `pkg-config --static --libs glfw3`
+g++ -g -o ./bin/midge                                           \
+    `pkg-config --cflags glfw3`                                 \
+    -lGL -lX11 -lpthread -lXrandr -lXi -ldl                     \
+    -I./src                                                     \
+    -I/home/daniel/cling/src/include                            \
+    -I/home/daniel/cling/src/tools/cling/include                \
+    -I/home/daniel/cling/obj/include                            \
+    /home/daniel/cling/obj/lib/libcling.so                      \
+    /home/daniel/cling/obj/lib/libcling.so.5                    \
+    $(find . -type f -iregex ".*\.cpp")                         \
+    `pkg-config --static --libs glfw3`
 
 retval=$?
 if [ $retval -ne 0 ]; then
