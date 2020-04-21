80e8e49365e7f906fe15fadd1089acde3feca5d3
ShimmyShaman <blackforestcheesecake@protonmail.ch>
2020-04-22 06:37:37 +1200

    glfw more gone

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-    #/home/daniel/cling/obj/lib/libcling.so  \
-    #/home/daniel/cling/lib/libcling.so.5  \
-    #/home/daniel/cling/obj/lib/libcling.so.5.0.0  \
-    #-L/home/daniel/cling/obj/lib                                \
+#/home/daniel/cling/obj/lib/libcling.so  \
+#/home/daniel/cling/lib/libcling.so.5  \
+#/home/daniel/cling/obj/lib/libcling.so.5.0.0  \
+#-L/home/daniel/cling/obj/lib                                \
 
 echo
 echo Compiling
@@ -12,7 +12,7 @@ echo Compiling
 # export PKG_CONFIG_PATH='/usr/local/lib64/pkgconfig/'
 # g++ -g -o ./bin/midge                                           \
 #     `pkg-config --cflags glfw3`                                 \
-#     -lGL -lX11 -lpthread -lXrandr -lXi -ldl                     \
+# -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl   \
 #     -I./src                                                     \
 #     -I/home/daniel/cling/src/include                            \
 #     -I/home/daniel/cling/src/tools/cling/include                \
@@ -23,22 +23,16 @@ echo Compiling
 #     /home/daniel/cling/obj/lib/libLLVMSupport.a                 \
 #     $(find . -type f -iregex ".*\.cpp")                         \
 #     `pkg-config --static --libs glfw3`
-export LD_LIBRARY_PATH='/home/daniel/cling/obj/lib;/usr/local/lib64'
-export PKG_CONFIG_PATH='/usr/local/lib64/pkgconfig/'
-g++ -g -fPIC src/main/main.cpp                           \
-    src/opengl/glad/glad.cpp                          \
-    -I./src                                           \
-    -I./src/opengl/glad.h                             \
-    -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl   \
-    -I/home/daniel/cling/src/include                  \
-    -I/home/daniel/cling/src/tools/cling/include      \
-    -I/home/daniel/cling/src/tools/clang/include      \
-    -I/home/daniel/cling/obj/include                  \
-    /home/daniel/cling/obj/lib/libcling.so            \
-    -o bin/midge               
-    #`pkg-config --cflags glfw3` \                   
-    #`pkg-config --libs glfw3`
-    # ./dep/glad.o \
+# export LD_LIBRARY_PATH='/home/daniel/cling/obj/lib;/usr/local/lib64'
+# export PKG_CONFIG_PATH='/usr/local/lib64/pkgconfig/'
+g++ -g -fPIC src/main/main.cpp                        \
+-I./src/midge.h                                   \
+-I/home/daniel/cling/src/include                  \
+-I/home/daniel/cling/src/tools/cling/include      \
+-I/home/daniel/cling/src/tools/clang/include      \
+-I/home/daniel/cling/obj/include                  \
+/home/daniel/cling/obj/lib/libcling.so            \
+-o bin/midge
 
 # export LD_LIBRARY_PATH='/home/daniel/cling/obj/lib;/home/daniel/midge/dep'
 # g++ -fPIC src/main/main.cpp                           \
@@ -53,7 +47,7 @@ retval=$?
 if [ $retval -ne 0 ]; then
     echo "Compilation Failed : $retval"
 else
-	echo Running
+    echo Running
     echo "#######################"
     ./bin/midge
     retval=$?
