088769475e0b4a4a3385e388183c724df264ef70
adam-rasburn <blackforestcheesecake@protonmail.ch>
2020-07-09 07:29:30 +1200

    function_editor_handle_input core_function working

--- src/core/function_editor_handle_input.c
+++ src/core/function_editor_handle_input.c
@@ -1,64 +1,66 @@
-// void function_editor_handle_input(frame_time *elapsed, node *fedit, mc_input_event_v1 *event)
-// {
-//   printf("function_editor_handle_input-b\n");
+#include "core/midge_core.h"
 
-//   if (fedit->data.visual.hidden) {
-//     return;
-//   }
+void function_editor_handle_input_v1(frame_time *elapsed, mc_node_v1 *fedit, mc_input_event_v1 *event)
+{
+  printf("function_editor_handle_input-begin\n");
 
-//   function_editor_state *state = (function_editor_state *)fedit->extra;
+  if (fedit->data.visual.hidden) {
+    return;
+  }
 
-//   event->handled = true;
+  function_editor_state *state = (function_editor_state *)fedit->extra;
 
-//   printf("fehi-3\n");
-//   if (event->type == INPUT_EVENT_MOUSE_PRESS) {
-//     if (event->detail.mouse.button == MOUSE_BUTTON_SCROLL_DOWN) {
-//       ++state->line_display_offset;
-//     }
-//     else if (event->detail.mouse.button == MOUSE_BUTTON_SCROLL_UP) {
-//       --state->line_display_offset;
-//     }
-//   }
-//   else if (event->type == INPUT_EVENT_KEY_PRESS) {
-//     function_editor_handle_keyboard_input(fedit, event);
-//   }
-//   else {
-//     return;
-//   }
+  event->handled = true;
 
-//   printf("fehi-4\n");
-//   // Update all modified rendered lines
-//   for (int i = 0; i < FUNCTION_EDITOR_RENDERED_CODE_LINES; ++i) {
-//     if (i + state->line_display_offset >= state->text.lines_count) {
+  printf("fehi-3\n");
+  if (event->type == INPUT_EVENT_MOUSE_PRESS) {
+    if (event->detail.mouse.button == MOUSE_BUTTON_SCROLL_DOWN) {
+      ++state->line_display_offset;
+    }
+    else if (event->detail.mouse.button == MOUSE_BUTTON_SCROLL_UP) {
+      --state->line_display_offset;
+    }
+  }
+  else if (event->type == INPUT_EVENT_KEY_PRESS) {
+    function_editor_handle_keyboard_input(fedit, event);
+  }
+  else {
+    return;
+  }
 
-//       printf("fehi-5\n");
-//       if (!state->render_lines[i].text) {
-//         continue;
-//       }
+  printf("fehi-4\n");
+  // Update all modified rendered lines
+  for (int i = 0; i < FUNCTION_EDITOR_RENDERED_CODE_LINES; ++i) {
+    if (i + state->line_display_offset >= state->text.lines_count) {
 
-//       // printf("was:'%s' now:NULL\n", state->render_lines[i].text);
-//       free(state->render_lines[i].text);
-//       state->render_lines[i].text = NULL;
-//     }
-//     else {
-//       printf("fehi-6\n");
-//       if (state->render_lines[i].text &&
-//           !strcmp(state->render_lines[i].text, state->text.lines[i + state->line_display_offset])) {
-//         continue;
-//       }
+      printf("fehi-5\n");
+      if (!state->render_lines[i].text) {
+        continue;
+      }
 
-//       // printf("was:'%s' now:'%s'\n", state->render_lines[i].text, state->text.lines[i + state->line_display_offset]);
-//       // Update
-//       if (state->render_lines[i].text) {
-//         free(state->render_lines[i].text);
-//       }
-//       allocate_and_copy_cstr(state->render_lines[i].text, state->text.lines[i + state->line_display_offset]);
-//     }
+      // printf("was:'%s' now:NULL\n", state->render_lines[i].text);
+      free(state->render_lines[i].text);
+      state->render_lines[i].text = NULL;
+    }
+    else {
+      printf("fehi-6\n");
+      if (state->render_lines[i].text &&
+          !strcmp(state->render_lines[i].text, state->text.lines[i + state->line_display_offset])) {
+        continue;
+      }
 
-//     printf("fehi-7\n");
-//     state->render_lines[i].requires_render_update = true;
-//     fedit->data.visual.requires_render_update = true;
-//   }
+      // printf("was:'%s' now:'%s'\n", state->render_lines[i].text, state->text.lines[i + state->line_display_offset]);
+      // Update
+      if (state->render_lines[i].text) {
+        free(state->render_lines[i].text);
+      }
+      allocate_and_copy_cstr(state->render_lines[i].text, state->text.lines[i + state->line_display_offset]);
+    }
 
-//   return;
-// }
+    printf("fehi-7\n");
+    state->render_lines[i].requires_render_update = true;
+    fedit->data.visual.requires_render_update = true;
+  }
+
+  return;
+}
--- src/midge_main.c
+++ src/midge_main.c
@@ -5944,46 +5944,46 @@ int init_core_functions(mc_command_hub_v1 *command_hub)
 
   printf("hopeh\n");
   // MCcall(parse_and_process_core_function(command_hub, "function_editor_handle_input"));
-  mc_function_info_v1 *function_editor_handle_input_v1 = (mc_function_info_v1 *)malloc(sizeof(mc_function_info_v1));
-  MCcall(append_to_collection((void ***)&command_hub->global_node->functions, &command_hub->global_node->functions_alloc,
-                              &command_hub->global_node->function_count, (void *)function_editor_handle_input_v1));
-  {
-    function_editor_handle_input_v1->struct_id = NULL;
-    function_editor_handle_input_v1->name = "function_editor_handle_input";
-    function_editor_handle_input_v1->latest_iteration = 1U;
-    allocate_and_copy_cstr(function_editor_handle_input_v1->return_type.name, "void");
-    function_editor_handle_input_v1->return_type.deref_count = 0;
-    function_editor_handle_input_v1->parameter_count = 3;
-    function_editor_handle_input_v1->parameters =
-        (mc_parameter_info_v1 **)malloc(sizeof(void *) * function_editor_handle_input_v1->parameter_count);
-    function_editor_handle_input_v1->variable_parameter_begin_index = -1;
-    function_editor_handle_input_v1->struct_usage_count = 0;
-    function_editor_handle_input_v1->struct_usage = NULL;
-    allocate_and_copy_cstr(function_editor_handle_input_v1->mc_code, function_editor_handle_input_v1_code);
+  // mc_function_info_v1 *function_editor_handle_input_v1 = (mc_function_info_v1 *)malloc(sizeof(mc_function_info_v1));
+  // MCcall(append_to_collection((void ***)&command_hub->global_node->functions, &command_hub->global_node->functions_alloc,
+  //                             &command_hub->global_node->function_count, (void *)function_editor_handle_input_v1));
+  // {
+  //   function_editor_handle_input_v1->struct_id = NULL;
+  //   function_editor_handle_input_v1->name = "function_editor_handle_input";
+  //   function_editor_handle_input_v1->latest_iteration = 1U;
+  //   allocate_and_copy_cstr(function_editor_handle_input_v1->return_type.name, "void");
+  //   function_editor_handle_input_v1->return_type.deref_count = 0;
+  //   function_editor_handle_input_v1->parameter_count = 3;
+  //   function_editor_handle_input_v1->parameters =
+  //       (mc_parameter_info_v1 **)malloc(sizeof(void *) * function_editor_handle_input_v1->parameter_count);
+  //   function_editor_handle_input_v1->variable_parameter_begin_index = -1;
+  //   function_editor_handle_input_v1->struct_usage_count = 0;
+  //   function_editor_handle_input_v1->struct_usage = NULL;
+  //   allocate_and_copy_cstr(function_editor_handle_input_v1->mc_code, function_editor_handle_input_v1_code);
 
-    mc_parameter_info_v1 *field;
-    field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
-    function_editor_handle_input_v1->parameters[0] = field;
-    field->type_name = "frame_time";
-    field->type_version = 1U;
-    field->type_deref_count = 1;
-    field->name = "frameTime";
+  //   mc_parameter_info_v1 *field;
+  //   field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
+  //   function_editor_handle_input_v1->parameters[0] = field;
+  //   field->type_name = "frame_time";
+  //   field->type_version = 1U;
+  //   field->type_deref_count = 1;
+  //   field->name = "frameTime";
 
-    field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
-    function_editor_handle_input_v1->parameters[1] = field;
-    field->type_name = "node";
-    field->type_version = 1U;
-    field->type_deref_count = 1;
-    field->name = "fedit";
+  //   field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
+  //   function_editor_handle_input_v1->parameters[1] = field;
+  //   field->type_name = "node";
+  //   field->type_version = 1U;
+  //   field->type_deref_count = 1;
+  //   field->name = "fedit";
 
-    field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
-    function_editor_handle_input_v1->parameters[2] = field;
-    field->type_name = "mc_input_event_v1";
-    field->type_version = 1U;
-    field->type_deref_count = 1;
-    field->name = "event";
-  }
-  clint_process("function_editor_handle_input = &function_editor_handle_input_v1;");
+  //   field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
+  //   function_editor_handle_input_v1->parameters[2] = field;
+  //   field->type_name = "mc_input_event_v1";
+  //   field->type_version = 1U;
+  //   field->type_deref_count = 1;
+  //   field->name = "event";
+  // }
+  // clint_process("function_editor_handle_input = &function_editor_handle_input_v1;");
   printf("hopee\n");
 
   f = fopen("/home/jason/midge/src/midge_core_ui.c", "rb");
@@ -6005,32 +6005,7 @@ int init_core_functions(mc_command_hub_v1 *command_hub)
   clint_process("core_display_entry_handle_input = &core_display_entry_handle_input_v1;");
 
   MCcall(parse_and_process_core_function(command_hub, "special_update"));
-  // mc_function_info_v1 *special_update_definition_v1 = (mc_function_info_v1 *)malloc(sizeof(mc_function_info_v1));
-  // MCcall(append_to_collection((void ***)&command_hub->global_node->functions, &command_hub->global_node->functions_alloc,
-  //                             &command_hub->global_node->function_count, (void *)special_update_definition_v1));
-  // {
-  //   special_update_definition_v1->struct_id = NULL;
-  //   special_update_definition_v1->name = "special_update";
-  //   special_update_definition_v1->latest_iteration = 1U;
-  //   allocate_and_copy_cstr(special_update_definition_v1->return_type.name, "void");
-  //   special_update_definition_v1->return_type.deref_count = 0;
-  //   special_update_definition_v1->parameter_count = 1;
-  //   special_update_definition_v1->parameters =
-  //       (mc_parameter_info_v1 **)malloc(sizeof(void *) * special_update_definition_v1->parameter_count);
-  //   special_update_definition_v1->variable_parameter_begin_index = -1;
-  //   special_update_definition_v1->struct_usage_count = 0;
-  //   special_update_definition_v1->struct_usage = NULL;
-  //   allocate_and_copy_cstr(special_update_definition_v1->mc_code, "  printf(\"I'm a yankee-doodle\\n\");\n");
-
-  //   mc_parameter_info_v1 *field;
-  //   field = (mc_parameter_info_v1 *)malloc(sizeof(mc_parameter_info_v1));
-  //   special_update_definition_v1->parameters[0] = field;
-  //   field->type_name = "frame_time";
-  //   field->type_version = 1U;
-  //   field->type_deref_count = 1;
-  //   field->name = "frameTime";
-  // }
-  // clint_process("special_update = &special_update_v1;");
+  MCcall(parse_and_process_core_function(command_hub, "function_editor_handle_input"));
   printf("end:init_core_functions()\n");
   return 0;
 }
