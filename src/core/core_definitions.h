0598e7be570115c93c14c5010bc9ccebb32fdae5
adam-rasburn <blackforestcheesecake@protonmail.ch>
2020-08-17 12:12:40 +1200

    eos

--- src/core/core_definitions.h
+++ src/core/core_definitions.h
@@ -3,48 +3,208 @@
 #ifndef CORE_DEFINITIONS_H
 #define CORE_DEFINITIONS_H
 
-#include "core/midge_core.h"
+// #include "core/midge_core.h"
+
+typedef enum source_file_type {
+  SOURCE_FILE_NULL = 0,
+  SOURCE_FILE_NODE,
+  SOURCE_FILE_MC_DEFINITIONS,
+  SOURCE_FILE_TEXT,
+  SOURCE_FILE_EXCLUSIVE_MAX = 100,
+} source_file_type;
+
+typedef enum source_definition_type {
+  SOURCE_DEFINITION_NULL = SOURCE_FILE_EXCLUSIVE_MAX,
+  SOURCE_DEFINITION_FUNCTION,
+  SOURCE_DEFINITION_STRUCT,
+  SOURCE_DEFINITION_ENUMERATION,
+} source_definition_type;
+
+typedef enum node_type {
+  NODE_TYPE_NONE = 1,
+  NODE_TYPE_GLOBAL_ROOT,
+  NODE_TYPE_CONSOLE_APP,
+  NODE_TYPE_VISUAL_APP,
+  NODE_TYPE_VISUAL,
+  NODE_TYPE_ABSTRACT,
+} node_type;
 
 typedef struct struct_id {
   const char *identifier;
   unsigned short version;
 } struct_id;
 
+struct source_file_info;
+struct struct_info;
+struct function_info;
+struct enumeration_info;
+typedef struct source_definition {
+  struct_id *type_id;
+  source_definition_type type;
+  source_file_info *source_file;
+  union {
+    void *p_data;
+    struct_info *structure_info;
+    function_info *func_info;
+    enumeration_info *enum_info;
+  } data;
+  char *code;
+} source_definition;
+
+typedef struct source_file_info {
+  struct_id *type_id;
+  char *filepath;
+  struct {
+    unsigned int alloc;
+    unsigned int count;
+    source_definition **items;
+  } definitions;
+} source_file_info;
+
+typedef struct enum_member {
+  struct_id *type_id;
+  const char *identity;
+  int value;
+} enum_member;
+
+typedef struct enumeration_info {
+  struct_id *type_id;
+  source_definition *source;
+  const char *name;
+  unsigned int latest_iteration;
+  struct {
+    unsigned int count, alloc;
+    enum_member **items;
+  } members;
+} enumeration_info;
+
+typedef struct field_info {
+  struct_id *type_id;
+  const char *type_name;
+  struct_info *type;
+  unsigned int type_version;
+  unsigned int type_deref_count;
+  const char *name;
+} field_info;
+
+typedef struct struct_info {
+  struct_id *type_id;
+  source_definition *source;
+  char *name;
+  unsigned int version;
+  char *declared_name;
+  unsigned int field_count;
+  field_info **fields;
+  const char *sizeof_cstr;
+} struct_info;
+
+typedef struct parameter_info {
+  struct_id *type_id;
+  bool is_function_pointer;
+  union {
+    struct {
+      const char *type_name;
+      const char *declared_type;
+      unsigned int type_version;
+    };
+    struct {
+      char *function_type;
+      char *full_function_pointer_declaration;
+    };
+  };
+  unsigned int type_deref_count;
+  const char *name;
+} parameter_info;
+
+typedef struct function_info {
+  struct_id *type_id;
+  source_definition *source;
+  const char *name;
+  unsigned int latest_iteration;
+  int (**ptr_declaration)(int, void **);
+  struct {
+    char *name;
+    unsigned int deref_count;
+  } return_type;
+  // char *code;
+  unsigned int parameter_count;
+  parameter_info **parameters;
+  int variable_parameter_begin_index;
+  unsigned int struct_usage_count;
+  struct_info **struct_usage;
+} function_info;
+
+typedef struct event_handler_array {
+  unsigned int event_type;
+  unsigned int count;
+  unsigned int alloc;
+  int (***handlers)(int, void **);
+} event_handler_array;
+
+typedef struct global_root_data {
+  struct {
+    unsigned int alloc, count;
+    function_info **items;
+  } functions;
+  struct {
+    unsigned int alloc, count;
+    struct_info **items;
+  } structs;
+  struct {
+    unsigned int alloc, count;
+    enumeration_info **items;
+  } enumerations;
+  struct {
+    unsigned int alloc, count;
+    event_handler_array **items;
+  } event_handlers;
+} global_root_data;
+
 typedef struct node {
-  struct_id *struct_id;
+  struct_id *type_id;
+  node_type type;
   const char *name;
 
   node *parent;
-  unsigned int children_alloc;
-  unsigned int child_count;
-  node **children;
+  struct {
+    unsigned int alloc, count;
+    node **items;
+  } children;
 
   void *data;
-};
+} node;
 
-//   unsigned int functions_alloc;
-//   unsigned int function_count;
-//   mc_function_info_v1 **functions;
-//   unsigned int structs_alloc;
-//   unsigned int struct_count;
-//   mc_struct_info_v1 **structs;
+// typedef struct command_hub {
+//   struct_id *type_id;
+//   node *global_node;
+//   node *nodespace;
+//   // mc_void_collection_v1 *render_sequence_pool;
 
-//   node_type type;
-//   union {
-//     node_visual_info visual;
-//     struct {
-//       uint image_resource_uid;
-//       int (*render_delegate)(int, void **);
-//     } global_root;
-//   } data;
+//   // char *selected_text; TODO for xcb implicit pasting
+//   char *clipboard_text;
 
-//   struct {
-//     uint alloc, count;
-//     event_handler_array **items;
-//   } event_handlers;
+//   // struct {
+//   //   resource_queue *resource_queue;
+//   //   render_queue *render_queue;
+//   // } renderer;
+//   // struct {
+//   //   uint count, allocated;
+//   //   update_callback_timer **callbacks;
+//   // } update_timers;
+//   // struct {
+//   //   uint count, alloc;
+//   //   mc_source_file_info_v1 **items;
+//   // } source_files;
+//   // mc_process_unit_v1 *process_matrix;
+//   // mc_workflow_process_v1 *focused_workflow;
+//   // unsigned int scripts_alloc;
+//   // unsigned int scripts_count;
+//   // void **scripts;
+//   unsigned int uid_counter;
+//   // mc_process_action_v1 *demo_issue;
+//   // mc_interactive_console_v1 *interactive_console;
 
-//   void *extra;
-}
-mc_node_v1;
+//   // unsigned int error_definition_index;
+// } mc_command_hub_v1;
 
 #endif // CORE_DEFINITIONS_H
--- src/core/core_source_loader.c
+++ src/core/core_source_loader.c
@@ -7,25 +7,25 @@ int _mcl_obtain_core_source_information(void **p_core_source_info)
   const int STRUCT = 11;
   const int FUNCTION = 12;
   const int ENUM = 13;
-  struct core_structure_info {
+  struct core_object_info {
     int type;
     const char *identity;
     const char *filepath;
   };
 
   const int MAX_OBJECTS_COUNT = 50;
-  struct core_structure_info *objects =
-      (struct core_structure_info *)malloc(sizeof(struct core_structure_info) * MAX_OBJECTS_COUNT);
+  struct core_object_info *objects =
+      (struct core_object_info *)malloc(sizeof(struct core_object_info) * MAX_OBJECTS_COUNT);
   int objects_index = 0;
 
   // Set
   objects[objects_index].type = STRUCT;
   objects[objects_index].identity = "struct_id";
-  objects[objects_index++].identity = "core/core_definitions.h";
+  objects[objects_index++].filepath = "src/core/core_definitions.h";
 
   objects[objects_index].type = STRUCT;
   objects[objects_index].identity = "node";
-  objects[objects_index++].identity = "core/core_definitions.h";
+  objects[objects_index++].filepath = "src/core/core_definitions.h";
 
   objects[objects_index].type = 0;
   if (objects_index >= MAX_OBJECTS_COUNT) {
@@ -36,7 +36,7 @@ int _mcl_obtain_core_source_information(void **p_core_source_info)
   return 0;
 }
 
-int _mcl_read_all_file_text(char *filepath, char **contents)
+int _mcl_read_all_file_text(const char *filepath, char **contents)
 {
   // Load the text from the core functions directory
   FILE *f = fopen(filepath, "rb");
@@ -54,118 +54,258 @@ int _mcl_read_all_file_text(char *filepath, char **contents)
   return 0;
 }
 
-// int _mcl_find_sequence_in_text_ignoring_empty_text(const char *text, const char *prefix, const char *middle,
-//                                                    const char *postfix, int *index)
-// {
-
-//   for (int i = 0;; ++i) {
-
-//     // Prefix
-//     int s;
-//     for (s = 0;; ++s) {
-//       if(prefix[s] == '\0'){
-//         break;
-//       }
-//       if(text[i + s] != prefix[s]) {
-//         s = -1;
-//         break;
-//       }
-//     }
-//     if(s < 0){
-//       continue;
-//     }
-//     while(text[i + ])
-//   }
-
-//   MCerror(60, "Could not find '%s'>'%s'>'%s'", prefix, middle, postfix);
-// }
-
-int _mcl_load_core_temp_struct(const char *filepath, const char *identity)
+int _mcl_find_sequence_in_text_ignoring_empty_text(const char *text, const char *first, const char *second,
+                                                   const char *third, int *index)
 {
-  char *file_text;
-  MCcall(_mcl_read_all_file_text(filepath, &file_text));
+  for (int i = 0;; ++i) {
 
-  int index;
-  MCcall(_mcl_find_sequence_in_text_ignoring_empty_text(file_text, "typedef struct", identity, "", &index));
+    if (text[i] == '\0') {
+      break;
+    }
+
+    // First
+    int s;
+    for (s = 0; first; ++s) {
+      if (first[s] == '\0') {
+        break;
+      }
+      if (text[i + s] != first[s]) {
+        s = -1;
+        break;
+      }
+    }
+    if (s < 0) {
+      continue;
+    }
+
+    // Second
+    int t = s;
+    for (s = 0; second; ++s) {
+      if (second[s] == '\0') {
+        break;
+      }
+      if (text[i + t + s] != second[s]) {
+        s = -1;
+        break;
+      }
+    }
+    if (s < 0) {
+      continue;
+    }
+
+    // Third
+    t = t + s;
+    for (s = 0; third; ++s) {
+      if (third[s] == '\0') {
+        break;
+      }
+      if (text[i + t + s] != third[s]) {
+        s = -1;
+        break;
+      }
+    }
+    if (s < 0) {
+      continue;
+    }
+
+    *index = i;
+    return 0;
+  }
+
+  MCerror(60, "Could not find '%s'>'%s'>'%s'", first, second, third);
 }
 
-int _mcl_load_core_temp_source(void *p_core_source_info)
+int _mcl_load_core_temp_struct(void *p_core_source_info, int source_index)
 {
   const int STRUCT = 11;
   const int FUNCTION = 12;
   const int ENUM = 13;
-  struct core_structure_info {
+  struct core_object_info {
     int type;
     const char *identity;
     const char *filepath;
   };
+  struct core_object_info *core_objects = (struct core_object_info *)p_core_source_info;
 
-  struct core_structure_info *core_source_objects = (struct core_structure_info *)p_core_source_info;
+  // Find and extract the definition text
+  char *file_text;
+  MCcall(_mcl_read_all_file_text(core_objects[source_index].filepath, &file_text));
 
-  int objects_index = 0;
-  while (core_source_objects[objects_index].type) {
-    switch (core_source_objects[objects_index].type) {
-    case STRUCT:
-      MCcall(_mcl_load_core_temp_struct(core_source_objects[objects_index].filepath,
-                                        core_source_objects[objects_index].identity));
-      break;
-    default:
-      MCerror(60, "Unsupported:%i", core_source_objects[objects_index].type);
-      break;
+  int index;
+  MCcall(_mcl_find_sequence_in_text_ignoring_empty_text(file_text, "typedef struct ",
+                                                        core_objects[source_index].identity, NULL, &index));
+
+  int end_index;
+  MCcall(_mcl_find_sequence_in_text_ignoring_empty_text(file_text, "} ", core_objects[source_index].identity, ";",
+                                                        &end_index));
+  end_index += 2 + strlen(core_objects[source_index].identity) + 1;
+
+  c_str *definition;
+  MCcall(init_c_str(&definition));
+  MCcall(append_to_c_strn(definition, file_text + index, end_index - index));
+
+  // Convert it
+  for (int i = 0; i < definition->len; ++i) {
+    for (int o = 0; o <= source_index; ++o) {
+      int n = strlen(core_objects[o].identity);
+      if (!strncmp(definition->text + i, core_objects[o].identity, n)) {
+        MCcall(insert_into_c_str(definition, "mc_core_v_", i));
+        i += 10 + n - 1;
+      }
+    }
+  }
+
+  // printf("newdef:\n%s||\n", definition->text);
+  // MCerror(159, "next");
+
+  clint_declare(definition->text);
+
+  return 0;
+}
+
+int _mcl_load_core_temp_source(void *p_core_source_info)
+{
+  // const int STRUCT = 11;
+  // const int FUNCTION = 12;
+  // const int ENUM = 13;
+  // struct core_object_info {
+  //   int type;
+  //   const char *identity;
+  //   const char *filepath;
+  // };
+  // struct core_object_info *core_objects = (struct core_object_info *)p_core_source_info;
+
+  // int objects_index = 0;
+  // while (core_objects[objects_index].type) {
+  //   printf("%i:%s:%s\n", core_objects[objects_index].type, core_objects[objects_index].filepath,
+  //          core_objects[objects_index].identity);
+  //   switch (core_objects[objects_index].type) {
+  //   case STRUCT:
+  //     MCcall(_mcl_load_core_temp_struct(p_core_source_info, objects_index));
+  //     break;
+  //   default:
+  //     MCerror(60, "Unsupported:%i", core_objects[objects_index].type);
+  //     break;
+  //   }
+
+  //   ++objects_index;
+  // }
+
+  const char *core_objects[] = {
+      "node", "global_root_data", "function_info", "struct_info", "enumeration_info", NULL,
+  };
+
+  const char *source_files[] = {
+      "src/core/core_definitions.h",
+      NULL,
+  };
+
+  c_str *src;
+  MCcall(init_c_str(&src));
+  for (int a = 0; source_files[a]; ++a) {
+
+    char *file_text;
+    MCcall(_mcl_read_all_file_text(source_files[a], &file_text));
+    MCcall(set_c_str(src, file_text));
+    free(file_text);
+
+    for (int i = 0; i < src->len; ++i) {
+      for (int o = 0; core_objects[o]; ++o) {
+        int n = strlen(core_objects[o]);
+        if (!strncmp(src->text + i, core_objects[o], n)) {
+          MCcall(insert_into_c_str(src, "mc_core_v_", i));
+          i += 10 + n - 1;
+        }
+      }
     }
 
-    ++objects_index;
+    printf("def:\n%s||\n", src->text);
+    MCcall(clint_declare(src->text));
   }
 
   return 0;
 }
 
-int _mcl_init_core_data(void **p_command_hub)
+int _mcl_init_core_data(void **p_global_root)
 {
-  char buf[1280];
+  char buf[6400];
   sprintf(buf,
           "{"
-          "  mc_node_v1 *global = (mc_node_v1 *)calloc(sizeof(mc_node_v1), 1);"
+          "  mc_core_v_node *global = (mc_core_v_node *)calloc(sizeof(mc_core_v_node), 1);"
+          "  global->type = NODE_TYPE_GLOBAL_ROOT;"
           "  global->name = \"global\";"
           "  global->parent = NULL;"
-          "  global->functions.alloc = 40;"
-          "  global->functions.count = 0;"
-          "  global->functions.items = (mc_function_info_v1 **)calloc(sizeof(mc_function_info_v1 *), "
-          "                             global->functions_alloc);"
-          "  global->structs.alloc = 40;"
-          "  global->struct.count = 0;"
-          "  global->structs.items = (mc_struct_info_v1 **)calloc(sizeof(mc_struct_info_v1 *), global->structs_alloc);"
           "  global->children.alloc = 40;"
           "  global->children.count = 0;"
-          "  global->children.items = (mc_node_v1 **)calloc(sizeof(mc_node_v1 *), global->children_alloc);"
-          "  global->event_handlers.alloc = 0;"
-          "  global->event_handlers.count = 0;"
+          "  global->children.items = (mc_core_v_node **)calloc(sizeof(mc_core_v_node *), global->children.alloc);"
           ""
-          "  mc_command_hub_v1 *command_hub = (mc_command_hub_v1 *)calloc(sizeof(mc_command_hub_v1), 1);"
-          "  command_hub->global_node = global;"
-          "  command_hub->nodespace = global;"
-          "  command_hub->uid_counter = 2000;"
-          "  command_hub->source_files.alloc = 0;"
-          "  command_hub->source_files.count = 0;"
-          "  allocate_and_copy_cstr(command_hub->clipboard_text, "
-          ");"
-
-          "  *(void **)(%p) = (void *)command_hub;"
+          "  mc_core_v_global_root_data *global_root_data = (mc_core_v_global_root_data "
+          "*)malloc(sizeof(mc_core_v_global_root_data ));"
+          "  global->data = global_root_data;"
+          ""
+          "  global_root_data->functions.alloc = 100;"
+          "  global_root_data->functions.count = 0;"
+          "  global_root_data->functions.items = (mc_core_v_function_info **)calloc(sizeof(mc_core_v_function_info *),"
+          "                                         global_root_data->functions.alloc);"
+          ""
+          "  global_root_data->structs.alloc = 30;"
+          "  global_root_data->structs.count = 0;"
+          "  global_root_data->structs.items = (mc_core_v_struct_info **)calloc(sizeof(mc_core_v_struct_info *),"
+          "                                       global_root_data->structs.alloc);"
+          ""
+          "  global_root_data->enumerations.alloc = 20;"
+          "  global_root_data->enumerations.count = 0;"
+          "  global_root_data->enumerations.items = (mc_core_v_enumeration_info "
+          "**)calloc(sizeof(mc_core_v_enumeration_info *), global_root_data->enumerations.alloc);"
+          ""
+          "  global_root_data->event_handlers.alloc = 0;"
+          "  global_root_data->event_handlers.count = 0;"
+          ""
+          "  *(void **)(%p) = (void *)global_root_data;"
           "}",
-          p_command_hub);
-  clint_process(buf);
+          p_global_root);
+  MCcall(clint_process(buf));
+  printf("here-8\n");
 
   return 0;
 }
 
-int _mcl_load_core_mc_source(void *command_hub, void *p_core_source_info) { return 0; }
+int _mcl_load_core_mc_source(void *command_hub, void *p_core_source_info)
+{
+  const char *source_files[] = {
+      "src/core/core_definitions.h",
+      NULL,
+  };
 
-int _mcl_load_app_mc_source(void *command_hub) { return 0; }
+  char buf[512];
+  for (int i = 0; source_files[i]; ++i) {
+    sprintf(buf, "MCcall(mc_core_v_instantiate_all_definitions_from_file(\"%s\"));", source_files[i]);
+    MCcall(clint_process(buf));
+  }
+
+  return 0;
+}
+
+int _mcl_load_app_mc_source(void *command_hub)
+{
+  const char *source_files[] = {
+      "src/index_functions.c",
+      NULL,
+  };
+
+  char buf[512];
+  for (int i = 0; source_files[i]; ++i) {
+    sprintf(buf, "MCcall(instantiate_all_definitions_from_file(\"%s\"));", source_files[i]);
+    MCcall(clint_process(buf));
+  }
+
+  return 0;
+}
 
 int mcl_load_app_source(void **command_hub)
 {
-  void *p_core_source_info;
-  MCcall(_mcl_obtain_core_source_information(&p_core_source_info));
+  void *p_core_source_info = NULL;
+  // MCcall(_mcl_obtain_core_source_information(&p_core_source_info));
   MCcall(_mcl_load_core_temp_source(p_core_source_info));
 
   MCcall(_mcl_init_core_data(command_hub));
